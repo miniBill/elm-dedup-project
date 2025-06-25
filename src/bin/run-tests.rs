507fe2054507b64d1fab4775f92c014af9f4d1d1
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-06-25 15:40:49 +0200

    Multiple runners

--- src/bin/run-tests.rs
+++ src/bin/run-tests.rs
@@ -4,6 +4,7 @@ use std::{
     fs, io,
     path::{Path, PathBuf},
     process::{Command, Stdio},
+    sync::Arc,
     time::Duration,
 };
 use tokio::{sync::Mutex, task::JoinHandle, time::Instant};
@@ -12,8 +13,8 @@ use tokio::{sync::Mutex, task::JoinHandle, time::Instant};
 enum Error {
     IO(io::Error),
     SendPath(async_channel::SendError<PathBuf>),
-    SendDone(async_channel::SendError<Done>),
     Join(tokio::task::JoinError),
+    ColorEyre(color_eyre::Report),
     Other(String),
 }
 
@@ -29,15 +30,15 @@ impl From<async_channel::SendError<PathBuf>> for Error {
     }
 }
 
-impl From<async_channel::SendError<Done>> for Error {
-    fn from(e: async_channel::SendError<Done>) -> Self {
-        Error::SendDone(e)
-    }
-}
-
 impl From<tokio::task::JoinError> for Error {
     fn from(e: tokio::task::JoinError) -> Self {
         Error::Join(e)
+    }
+}
+
+impl From<color_eyre::Report> for Error {
+    fn from(e: color_eyre::Report) -> Self {
+        Error::ColorEyre(e)
     }
 }
 
@@ -62,21 +63,29 @@ struct Done {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let concurrency: i32 = 4;
+
     let (paths_sender, paths_receiver): (
         async_channel::Sender<PathBuf>,
         async_channel::Receiver<PathBuf>,
     ) = async_channel::unbounded();
     let (done_sender, done_receiver): (async_channel::Sender<Done>, async_channel::Receiver<Done>) =
         async_channel::unbounded();
-    let in_progress: Mutex<HashMap<PathBuf, Instant>> = Mutex::new(HashMap::new());
+    let in_progress: Arc<Mutex<HashMap<PathBuf, Instant>>> = Arc::new(Mutex::new(HashMap::new()));
     let mut dones: Vec<Done> = Vec::new();
 
+    let stopping: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
+
+    let walker_stopping: Arc<Mutex<bool>> = Arc::clone(&stopping);
     let walker: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
         let paths: Result<(), Error> = (async || {
             let repos = Path::new("repos");
             for author_root in read_dir(&repos)? {
                 for package_root in read_dir(author_root)? {
                     for version_root in read_dir(package_root)? {
+                        if *walker_stopping.lock().await {
+                            return Ok(());
+                        }
                         let tests: PathBuf = version_root.join("tests");
                         let elm_json: PathBuf = version_root.join("elm.json");
                         if tests.exists() && elm_json.exists() {
@@ -92,35 +101,53 @@ async fn main() -> Result<(), Error> {
         paths
     });
 
-    let tester: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
-        loop {
-            let version_root: PathBuf = match paths_receiver.recv_blocking() {
-                Ok(version_root) => version_root,
-                Err(async_channel::RecvError) => return Ok(()),
-            };
+    let mut testers: Vec<JoinHandle<Result<(), Error>>> = Vec::new();
+    for _i in 0..concurrency {
+        let stopping: Arc<Mutex<bool>> = Arc::clone(&stopping);
+        let paths_receiver: async_channel::Receiver<PathBuf> = paths_receiver.clone();
+        let done_sender: async_channel::Sender<Done> = done_sender.clone();
+        let in_progress: Arc<Mutex<HashMap<PathBuf, Instant>>> = in_progress.clone();
+        let tester: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
+            loop {
+                if *stopping.lock().await {
+                    return Ok(());
+                }
 
-            let start: Instant = Instant::now();
-            in_progress.lock().await.insert(version_root.clone(), start);
+                let version_root: PathBuf = match paths_receiver.recv_blocking() {
+                    Ok(version_root) => version_root,
+                    Err(async_channel::RecvError) => return Ok(()),
+                };
 
-            let res: Result<(bool, bool), Error> = check_tests_for(&version_root);
+                let start: Instant = Instant::now();
+                in_progress.lock().await.insert(version_root.clone(), start);
 
-            in_progress.lock().await.remove(&version_root);
+                let res: Result<(bool, bool), Error> = check_tests_for(&version_root);
 
-            let (elm_result, lamdera_result) = res?;
+                in_progress.lock().await.remove(&version_root);
 
-            done_sender
-                .send(Done {
-                    path: version_root,
-                    time: start.elapsed(),
-                    elm_result,
-                    lamdera_result,
-                })
-                .await?;
-        }
-    });
+                let (elm_result, lamdera_result) = res?;
 
+                // If we have a send error it's because we're done, no need to deal with it
+                let _ = done_sender
+                    .send(Done {
+                        path: version_root,
+                        time: start.elapsed(),
+                        elm_result,
+                        lamdera_result,
+                    })
+                    .await;
+            }
+        });
+        testers.push(tester);
+    }
+
+    let consumer_stopping = Arc::clone(&stopping);
     let consumer: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
         loop {
+            if *consumer_stopping.lock().await {
+                return Ok(());
+            }
+
             let done = match done_receiver.recv_blocking() {
                 Ok(done) => done,
                 Err(async_channel::RecvError) => return Ok(()),
@@ -130,9 +157,35 @@ async fn main() -> Result<(), Error> {
         }
     });
 
+    let tui: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
+        color_eyre::install()?;
+        let res: Result<(), Error> = (|| loop {
+            let mut terminal: ratatui::Terminal<_> = ratatui::init();
+
+            terminal.draw(|_frame: &mut ratatui::Frame| {})?;
+            match ratatui::crossterm::event::read()? {
+                ratatui::crossterm::event::Event::Key(_) => return Ok(()),
+                ratatui::crossterm::event::Event::FocusGained => {}
+                ratatui::crossterm::event::Event::FocusLost => {}
+                ratatui::crossterm::event::Event::Mouse(_mouse_event) => {}
+                ratatui::crossterm::event::Event::Paste(_) => {}
+                ratatui::crossterm::event::Event::Resize(_, _) => {}
+            }
+        })();
+
+        *stopping.lock().await = true;
+
+        ratatui::restore();
+
+        res
+    });
+
     walker.await??;
-    tester.await??;
+    for tester in testers {
+        tester.await??;
+    }
     consumer.await??;
+    tui.await??;
 
     Ok(())
 }
